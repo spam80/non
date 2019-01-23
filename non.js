e7eab5f476ca593a0714a2e0b852d13aba5405e2
spam80 <45275318+spam80@users.noreply.github.com>
2019-01-23 21:31:50 +0100

    Update non.js

--- non.js
+++ non.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js'),
     client = new Discord.Client({sisableEveryone: true})
     console.log("دوز دوز,");
 client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("529732648533491722");
+const mohamed= member.guild.channels.get("535448350556946436");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`**# WeLCoMe To , Magic ' Server :1_: **`), 4000)        
+setTimeout(() => mohamed.send(`**# WeLCoMe To , Magic ' Server :rose: **`), 4000)        
 }
 });
 client.login(process.env.TOKEN);
