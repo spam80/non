bcd87dca4df9d8758127cfb93838f635be50a730
spam80 <45275318+spam80@users.noreply.github.com>
2019-01-26 14:25:26 +0100

    Update non.js

--- non.js
+++ non.js
@@ -5,7 +5,7 @@ client.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("535448350556946436");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`**# WeLCoMe To , Magic ' Server :rose: **`), 4000)        
+setTimeout(() => mohamed.send(`**# WeLCoMe To , Magic ' Server :rose: ${member}  **`), 4000)        
 }
 });
 client.login(process.env.TOKEN);
